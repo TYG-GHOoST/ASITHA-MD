c73a31b3b951992e493c73df2ddcd3edc5b2182d
TYG-GHOoST <atangelyt@gmail.com>
2024-10-12 13:14:01 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=znZVlCwD#GZ-YHXQStWAKT2Ex8VcD0j84OM6HseW-pQNs_7Q8oDs",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
 };
